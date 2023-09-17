0396be0f53dcb9a0a012f2482c1408e4ebf61e8e
SubhaChandiran <jerrysubash@gmail.com>
2023-09-17 18:28:38 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@
 // javascript function
 const App = () => {
   const now = new Date();
+  const a = 20;
+  const b = 20;
+  console.log(now, a + b);
   return (
     <div>
-      <p>Hello World!, it is{ now.toString() }</p>
+      <p>Hello World!, it is{now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b} 
+      </p> <br />
     </div>
   )
 }
